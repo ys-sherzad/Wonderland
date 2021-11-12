fc915b8586f0d080f53e26fd406c7e4754975c56
YasirSherzad <ys.sherzad@gmail.com>
2021-11-12 23:43:13 +0430

    add data

--- src/data.js
+++ src/data.js
@@ -28,4 +28,90 @@ export const stories = [
         profileImg: '',
         bgImage: '',
     }
-]
+];
+
+
+export const categories = [
+    {
+        name: 'Popular',
+    },
+    {
+        name: 'Following',
+    },
+    {
+        name: 'New',
+    },
+    {
+        name: 'Beach',
+    },
+    {
+        name: 'Hiking',
+    },
+    {
+        name: 'Cycling',
+    },
+    {
+        name: 'Mountain',
+    },
+    {
+        name: 'Historical',
+    }
+];
+
+export const posts = [
+    {
+        user: {
+            name: 'Amanda',
+            profImage: require('../assets/images/image1.png'),
+        },
+        name: 'Padar Island, Caribbean Sensation in Eastern Indonasia',
+        bgImage: require('../assets/images/rizknas-unsplash.jpg'),
+        date: 'June, 12 2021',
+        likesCount: '2,145',
+        commentsCount: '543',
+    },
+    {
+        user: {
+            name: 'Malika Hernn',
+            profImage: require('../assets/images/image2.png'),
+        },
+        name: 'Top 7 Destinations at Mount Bromo!',
+        bgImage: require('../assets/images/rizknas-unsplash.jpg'),
+        date: 'June, 11 2021',
+        likesCount: '1,623',
+        commentsCount: '656',
+    },
+    {
+        user: {
+            name: 'Daniel',
+            profImage: require('../assets/images/image3.png'),
+        },
+        name: 'All the way west in Ireland is one of the country’s most scenic counties',
+        bgImage: require('../assets/images/rizknas-unsplash.jpg'),
+        date: 'June, 11 2021',
+        likesCount: '1,623',
+        commentsCount: '656',
+    },
+    {
+        user: {
+            name: 'Daniel',
+            profImage: require('../assets/images/image4.png'),
+        },
+        name: 'The Maldives, 1,000 coral islands',
+        bgImage: require('../assets/images/rizknas-unsplash.jpg'),
+        date: 'June, 11 2021',
+        likesCount: '1,623',
+        commentsCount: '656',
+    },
+    {
+        user: {
+            name: 'Daniel',
+            profImage: require('../assets/images/image1.png'),
+        },
+        name: `Tahiti's most famous island and the underwater adventures`,
+        bgImage: require('../assets/images/rizknas-unsplash.jpg'),
+        date: 'June, 11 2021',
+        likesCount: '1,623',
+        commentsCount: '656',
+    },
+];
