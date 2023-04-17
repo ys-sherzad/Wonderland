deb3e53a8cfe83c864e3fbd7e9cc8d67537d5a45
Yasir Sherzad <ys.sherzad@gmail.com>
2023-04-17 21:59:03 +0200

    add loader on app start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
+  View,
   SafeAreaView,
   StatusBar,
+  ActivityIndicator
 } from 'react-native';
 import Home from './home';
 
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex:1}}>
       <StatusBar barStyle={'dark-content'} />
-      <Home />
+      {isLoading && (
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator size={'small'} color="#000" />
+        </View>
+      )}
+      {!isLoading && (<Home />)}
     </SafeAreaView>
   );
 };
